27396f889ebfd55797a071b3f18b4a5bd589ea8a
cmalven <chris@malven.co>
2022-04-05 10:20:03 -0500

    Rebuild vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
+import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 import viteRestart from 'vite-plugin-restart';
 import createPartials from './vite-create-partials.js';
 
 // https://vitejs.dev/config/
-export default ({ command }) => {
-  return {
-    base: command === 'serve' ? '' : '/dist/',
-    build: {
-      emptyOutDir: true,
-      manifest: true,
-      outDir: './web/dist/',
-      rollupOptions: {
-        input: './src/scripts/main.js',
+
+export default defineConfig({
+  base: '/dist/',
+  build: {
+    emptyOutDir: true,
+    manifest: true,
+    outDir: './web/dist/',
+    rollupOptions: {
+      input: {
+        script: './src/scripts/main.js',
+        styles: './src/styles/main.scss',
       },
     },
-    plugins: [
-      createPartials(),
-      legacy({
-        targets: ['defaults', 'not IE 11'],
-      }),
-      viteRestart({
-        reload: ['./templates/**/*'],
-      }),
-    ],
-  };
-};
+  },
+  plugins: [
+    createPartials(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    viteRestart({
+      reload: ['./templates/**/*'],
+    }),
+  ],
+});
