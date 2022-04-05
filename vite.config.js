9ce7230505956b4d7a9025ed3d8e08b8053c28d2
cmalven <chris@malven.co>
2022-04-05 09:15:34 -0500

    Remove unused partial.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import legacy from '@vitejs/plugin-legacy';
 import ViteRestart from 'vite-plugin-restart';
 import createPartials from "./vite-create-partials.js";
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ command }) => {
