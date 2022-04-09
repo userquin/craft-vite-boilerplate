453ca4aec631364f0930d24459af71bbd03cad15
cmalven <chris@malven.co>
2022-04-09 11:11:13 -0500

    Remove dotenv package.

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import viteRestart from 'vite-plugin-restart';
 import craftPartials from './vite-plugin-craft-partials.js';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
-import { config } from 'dotenv';
-config();
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  base: '/dist/',
-  publicDir: './web/',
-  server: {
-    port: process.env.VITE_DEV_PORT || 3000,
-  },
-  build: {
-    emptyOutDir: true,
-    manifest: true,
-    outDir: './web/dist/',
-    assetsDir: './web/static/',
-    rollupOptions: {
-      input: {
-        scripts: './src/scripts/main.js',
-        styles: './src/styles/main.scss',
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  
+  return {
+    base: '/dist/',
+    publicDir: './web/',
+    server: {
+      port: process.env.VITE_DEV_PORT || 3000,
+    },
+    build: {
+      emptyOutDir: true,
+      manifest: true,
+      outDir: './web/dist/',
+      assetsDir: './web/static/',
+      rollupOptions: {
+        input: {
+          scripts: './src/scripts/main.js',
+          styles: './src/styles/main.scss',
+        },
       },
     },
-  },
-  plugins: [
-    sassGlobImports(),
-    craftPartials(),
-    viteRestart({
-      reload: ['./templates/**/*'],
-    }),
-  ],
+    plugins: [
+      sassGlobImports(),
+      craftPartials(),
+      viteRestart({
+        reload: ['./templates/**/*'],
+      }),
+    ],
+  };
 });
